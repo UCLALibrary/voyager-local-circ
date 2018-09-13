fc2a2cdd1da8eacf3a49111ebe261b8ceeb2eb3a
David Rickard <drickard1967@library.ucla.edu>
2018-09-13 10:34:05 -0700

    DITPP-278 script and query to ID returned items with libbill invoice

---
+++ update_libbill_refunds.sql
@@ -0,0 +1,7 @@
+UPDATE ucladb.fine_fee_transactions SET trans_note = '09/10/18 Credited in LibBill           CS008737 Line 4' WHERE fine_fee_trans_id = 619814;
+UPDATE ucladb.fine_fee_transactions SET trans_note = '09/10/18 Credited in LibBill           CS008737 Line 6' WHERE fine_fee_trans_id = 619808;
+UPDATE ucladb.fine_fee_transactions SET trans_note = '09/10/18 Credited in LibBill           CS008455 Line 9' WHERE fine_fee_trans_id = 619816;
+UPDATE ucladb.fine_fee_transactions SET trans_note = '09/10/18 Credited in LibBill           CS008455 Line 11' WHERE fine_fee_trans_id = 619812;
+UPDATE ucladb.fine_fee_transactions SET trans_note = '09/10/18 Credited in LibBill           LW007845 Line 2' WHERE fine_fee_trans_id = 619806;
+UPDATE ucladb.fine_fee_transactions SET trans_note = '09/10/18 Credited in LibBill           LW007845 Line 3' WHERE fine_fee_trans_id = 619810;
+UPDATE ucladb.fine_fee_transactions SET trans_note = '09/10/18 Credited in LibBill           LW007845 Line 4' WHERE fine_fee_trans_id = 619818;
