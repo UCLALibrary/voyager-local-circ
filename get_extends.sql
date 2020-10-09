280310efad7cce9bf608d8fabbd6624be05d57c9
Andy Kohler <akohler@library.ucla.edu>
2020-10-08 20:20:50 -0700

    VBT-1681: Update due date extension query with new date and comments

--- get_extends.sql
+++ get_extends.sql
@@ -9,9 +9,11 @@ FROM
     left join ucladb.patron_barcode pb on ct.patron_id = pb.patron_id and pb.barcode_status = 1
     left join ucladb.item_barcode ib on ct.item_id = ib.item_id and ib.barcode_status = 1
 WHERE
-    current_due_date < to_date('2020-11-01','YYYY-MM-DD')
+    ct.current_due_date < to_date('2021-04-30','YYYY-MM-DD')
     AND p.expire_date >= sysdate
+	-- Exclude various CLICC equipment - see VBT-1681 for item type names
     and i.item_type_id not in (49,50,51,52,53,60,65,68,70,72,80,81,82,83,84,85)
+	-- Exclude Lost--Library Applied and Lost--System Applied items
     and not exists (select * from ucladb.item_status ist where ct.item_id = ist.item_id and
         item_status in (13, 14))
 ;
